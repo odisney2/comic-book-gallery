3c3f65eb09fceb200aac286df0faec72618d5286
O.J. Disney II <odisney2@gmail.com>
2018-05-31 11:45:45 -0400

    Added Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,17 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                // return new RedirectResult("/");
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller");
-            //return new ContentResult()
-            //{
-             //   Content = ""
-            //};
+            return View();
         } 
     }
 }  
